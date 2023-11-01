396a9b6cd719b9f51427d808414e0a8103268395
宋金来 <911908556@qq.com>
2023-11-01 09:32:14 +0800

    2023/11/1 Lay song create code!

---
+++ NS/etonet_split_order_ue.js
@@ -0,0 +1,109 @@
+/**
+ *  機能: 仕入伝票行制御
+ *  Author: 宋金来
+ *  Date : 2023/10/26
+ *
+ * @NApiVersion 2.1
+ * @NScriptType UserEventScript
+ */
+define(['N/record'],
+
+    (record) => {
+
+        /**
+         * Defines the function definition that is executed before record is submitted.
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.newRecord - New record
+         * @param {Record} scriptContext.oldRecord - Old record
+         * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
+         * @since 2015.2
+         */
+        const beforeSubmit = (scriptContext) => {
+            let newRec = scriptContext.newRecord;
+            let recordType = scriptContext.newRecord.type;
+            let type = scriptContext.type;
+            if (recordType != record.Type.PURCHASE_ORDER) return;
+            if (newRec.getValue({fieldId:"createdfrom"})) return;
+            if (type == 'create' ) {
+                let count = newRec.getLineCount({sublistId: "item"});
+                for (let i = 0; i < count; i++) {
+                    let itemType = newRec.getSublistValue({sublistId: "item", fieldId: "itemtype", line: i});
+                    let location = newRec.getSublistValue({sublistId: "item", fieldId: "location", line: i});
+                    if (itemType == "InvtPart" && !location) {
+                        throw "在庫品の場合は、入庫する倉庫を選択してください";
+                    }
+                }
+            }
+        }
+
+        /**
+         * Defines the function definition that is executed after record is submitted.
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.newRecord - New record
+         * @param {Record} scriptContext.oldRecord - Old record
+         * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
+         * @since 2015.2
+         */
+        const afterSubmit = (scriptContext) => {
+            let recordType = scriptContext.newRecord.type;
+            let recordId = scriptContext.newRecord.id;
+            let type = scriptContext.type;
+            log.audit("type",type );
+            try {
+                if (type == 'create' || type == 'dropship') {
+                    let newRec = record.load({type: recordType, id: recordId, isDynamic: true});
+                    let count = newRec.getLineCount({sublistId: "item"});
+                    if (count <= 5) return;
+                    let num = count % 5 !== 0 ? Math.floor(count / 5)  : count / 5;
+                    let line = 1;
+                    for (let i = 0; i < num; i++) {
+                        let copyRec = record.copy({type: recordType, id: recordId, isDynamic: true});
+                        let count = copyRec.getLineCount({sublistId: "item"});
+                        for (let j = count -1; j >= 0; j--) {
+                            // let itemType =  copyRec.getSublistValue({sublistId:"item",fieldId:"itemtype",line:j})
+                            let currentLine = j;
+                            let lineStart = line * 5;
+                            let lineEnd = line * 5 + 4;
+                                // if (itemType != 'InvtPart' && itemType != 'NonInvtPart'){
+                                //     copyRec.removeLine({sublistId: "item", line: j});
+                                //     lineStart =  lineStart + 1 ;
+                                //     lineEnd = lineEnd +1;
+                                // }
+                            if (lineStart > currentLine || lineEnd < currentLine ) {
+                                    copyRec.removeLine({sublistId: "item", line: j})
+                            }
+                        }
+                        line++;
+                        copyRec.save();
+                    }
+
+                    // let  numCount = 0
+                    // for (let i = 0; i < count; i++) {
+                    //     let itemType =  newRec.getSublistValue({sublistId:"item",fieldId:"itemtype",line:i});
+                    //     if (  i < 5 ){
+                    //         if (itemType != 'InvtPart' && itemType != 'NonInvtPart'){
+                    //             numCount++;
+                    //         }
+                    //     }
+                    // }
+                    for (let i = count -1; i >= 0; i--) {
+                        // let itemType =  newRec.getSublistValue({sublistId:"item",fieldId:"itemtype",line:i})
+                        // if ( i > 4  + numCount) {
+                        if ( i > 4  ) {
+                            // if (itemType == 'InvtPart' || itemType == 'NonInvtPart'){
+                                newRec.removeLine({sublistId: "item", line: i})
+                            // }
+                        }
+                    }
+                    newRec.save();
+                }
+            } catch (e) {
+                log.audit("error", e);
+            }
+        }
+        return {
+            beforeSubmit,
+            afterSubmit
+        }
+
+    });
